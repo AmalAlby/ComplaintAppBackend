2b85ff4fec689f299aff8d1fcb5d0274d8ab03ba
AmalAlby <amalalbyn@gmail.com>
2022-12-11 17:13:15 +0530

    complaint app created

---
+++ src/main/java/com/nest/complaintappbackend/ComplaintappbackendApplication.java
@@ -0,0 +1,13 @@
+package com.nest.complaintappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplaintappbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplaintappbackendApplication.class, args);
+	}
+
+}
